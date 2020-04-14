1c84aaf299d903f0be4094cfbba80b758a0e0459
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-04-13 21:16:57 -0400

    relative path bug fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "/src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve("public"),
-    filename: "bundle.js"
+    path: path.resolve('public'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
@@ -12,9 +12,9 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 };
