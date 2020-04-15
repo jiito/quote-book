54f3dc9f860cdb3d8684b2ed2709d4a76af30900
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-04-14 22:13:59 -0400

    removed data reading

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import data from './testData.json';
 import App from './components/App';
 
-ReactDOM.render(<App quotes={data.quotes} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
