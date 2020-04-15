35f3354a809f2353a88293e4ca5384aa1009e990
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-04-14 22:12:34 -0400

    endpoint for quotes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import data from '../src/testData.json';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/quotes', (req, res) => {
   res.send({
-    key: [],
+    quotes: data.quotes,
   });
 });
 
