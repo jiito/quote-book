bb80a6e517fb08aa4d8fa47cfac4956390d3f615
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-04-14 22:14:20 -0400

    quote establishment in state

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 import Navigation from './Navigation';
-import Quote from './quote';
+import Quote from './Quote';
 
 class App extends React.Component {
   state = {
     pageHeader: 'Quotebook',
+    quotes: [],
   };
+
+  // eslint-disable-next-line class-methods-use-this
+  componentDidMount() {
+    axios
+      .get('/api/quotes')
+      .then((resp) => {
+        this.setState({
+          quotes: resp.data.quotes,
+        });
+      })
+      .catch(console.error());
+  }
 
   render() {
     return (
@@ -13,8 +27,8 @@ class App extends React.Component {
         <Navigation />
         <h2 className="text-center">{this.state.pageHeader}!</h2>
         <div>
-          {this.props.quotes.map((contest) => (
-            <Quote {...contest} />
+          {this.state.quotes.map((quote) => (
+            <Quote key={quote.id} {...quote} />
           ))}
         </div>
       </div>
