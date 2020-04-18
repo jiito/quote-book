b70349ac03c934cde0f23df2cdbabd8944c0703f
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-04-18 14:12:53 -0400

    mapping the quotes to object

--- api/index.js
+++ api/index.js
@@ -2,14 +2,15 @@ import express from 'express';
 import data from '../src/testData.json';
 
 const router = express.Router();
+const quotes = data.quotes.reduce((obj, quote) => {
+  obj[quote.id] = quote;
+  return obj;
+}, {});
 
 router.get('/quotes', (req, res) => {
   res.send({
     // map quotes to object
-    quotes: data.quotes.reduce((obj, quote) => {
-      obj[quote.id] = quote;
-      return obj;
-    }, {}),
+    quotes,
   });
 });
 
