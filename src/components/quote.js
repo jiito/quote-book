2def26c322c791beda64a1605e2761bbf01e6f7f
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-04-18 13:33:50 -0400

    stateful component

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Quote = (quote) => (
-  <div className="Quote">
-    <div className="quote-author">{quote.who}</div>
-    <div className="quote-string">{quote.what}</div>
-  </div>
-);
+class Quote extends Component {
+  state = {};
+
+  handleClick = () => {
+    console.log(this.props.who);
+  };
+
+  render() {
+    return (
+      <div className="Quote" onClick={this.handleClick}>
+        <div className="quote-author">{this.props.who}</div>
+        <div className="quote-string">{this.props.what}</div>
+      </div>
+    );
+  }
+}
 
 export default Quote;
