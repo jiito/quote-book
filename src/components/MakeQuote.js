adedcd51d390ae4daf40ff001626f4e89025ab93
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-04-21 08:21:44 -0400

    bootsrap styles

--- src/components/MakeQuote.js
+++ src/components/MakeQuote.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 class MakeQuote extends Component {
   render() {
     return (
-      <div className="MakeQuote panel">
+      <div className="MakeQuote panel panel-default">
         <div className="panel-heading">
           <h3 className="panel-title">Add a new quote </h3>
         </div>
         <div className="panel-body">
           <form>
-            <input
-              type="text"
-              placeholder="Who said it?"
-              ref="newWhoInput"
-              className="form-control"
-            />
-            <span className="input-group-btn">
-              <button type="submit" className="btn btn-info">
-                Submit
-              </button>
-            </span>
+            <div className="input-group">
+              <input
+                type="text"
+                placeholder="Who said it?"
+                ref="newWhoInput"
+                className="form-control"
+              />
+              <span className="input-group-btn">
+                <button type="submit" className="btn btn-info">
+                  Submit
+                </button>
+              </span>
+            </div>
           </form>
         </div>
       </div>
