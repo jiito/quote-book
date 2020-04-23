1492ecfd89b29da63431bf7da91a2b62535edff8
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-04-23 09:06:04 -0400

    add quote form

--- src/components/MakeQuote.js
+++ src/components/MakeQuote.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 
 class MakeQuote extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      who: '',
+      what: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    this.props.onQuoteSubmit(this.state.who, this.state.what);
+  }
+
+  handleChange(event) {
+    const key = event.target.name;
+    const { value } = event.target;
+    this.setState({
+      [key]: value,
+    });
+  }
+
   render() {
     return (
       <div className="MakeQuote panel panel-default">
@@ -8,12 +30,26 @@ class MakeQuote extends Component {
           <h3 className="panel-title">Add a new quote </h3>
         </div>
         <div className="panel-body">
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <div className="input-group">
+              <p>Who:</p>
               <input
                 type="text"
                 placeholder="Who said it?"
-                ref="newWhoInput"
+                name="who"
+                value={this.state.who}
+                onChange={this.handleChange}
+                className="form-control"
+              />
+            </div>
+            <div className="input-group">
+              <p>What:</p>
+              <input
+                type="text"
+                placeholder="What was said?"
+                name="what"
+                value={this.state.what}
+                onChange={this.handleChange}
                 className="form-control"
               />
               <span className="input-group-btn">
