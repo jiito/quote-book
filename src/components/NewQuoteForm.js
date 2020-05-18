69236c5abb4ec4fbd05b95d2d1e03c8f417250c8
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-05-17 22:44:05 -0400

    using custom hooks

--- src/components/NewQuoteForm.js
+++ src/components/NewQuoteForm.js
@@ -1,72 +1,51 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useInput } from '../hooks/input-hook';
 
-f;
+function NewQuoteForm(props) {
+  const { value: author, bind: bindAuthor, reset: resetAuthor } = useInput('');
+  const { value: quote, bind: bindQuote, reset: resetQuote } = useInput('');
 
-class MakeQuote extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      who: '',
-      what: '',
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    this.props.onQuoteSubmit(this.state.who, this.state.what);
+    props.onQuoteSubmit(author, quote);
+    resetAuthor();
+    resetQuote();
   }
 
-  handleChange(event) {
-    const key = event.target.name;
-    const { value } = event.target;
-    this.setState({
-      [key]: value,
-    });
-  }
-
-  render() {
-    return (
-      <div className="card">
-        <div className="card-body">
-          <h3 className="card-title">Add a new quote</h3>
-          <form>
-            <div className="form-group">
-              <p>Who:</p>
-              <input
-                type="text"
-                placeholder="Who said it?"
-                name="who"
-                value={this.state.who}
-                onChange={this.handleChange}
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <label className="control-label" htmlFor="what">
-                What:
-              </label>
-              <input
-                type="text"
-                id="what"
-                placeholder="What was said?"
-                name="what"
-                value={this.state.what}
-                onChange={this.handleChange}
-                className="form-control"
-              />
-            </div>
-            <span className="input-group-btn">
-              <button className="btn btn-primary" onClick={this.handleSubmit}>
-                Submit
-              </button>
-            </span>
-          </form>
-        </div>
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h3 className="card-title">Add a new quote</h3>
+        <form>
+          <div className="form-group">
+            <p>Who:</p>
+            <input
+              type="text"
+              placeholder="Who said it?"
+              name="author"
+              {...bindAuthor}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label className="control-label">What:</label>
+            <input
+              type="text"
+              placeholder="What was said?"
+              name="quote"
+              {...bindQuote}
+              className="form-control"
+            />
+          </div>
+          <span className="input-group-btn">
+            <button className="btn btn-primary" onClick={handleSubmit}>
+              Submit
+            </button>
+          </span>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default MakeQuote;
+export default NewQuoteForm;
