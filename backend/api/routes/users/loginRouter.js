982e95e58b6cbf059d9f11b7c91ce5e65071cd45
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-05-18 22:22:45 -0400

    creating JWT and signing

--- backend/api/routes/users/loginRouter.js
+++ backend/api/routes/users/loginRouter.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from '../../../../config';
 import validateLoginInput from '../../../validation/login';
 import { addNewUser } from '../../../src/controllers/UserController';
+import User from '../../../src/models/UserModel';
 
 const loginRouter = express.Router();
 
@@ -13,7 +17,42 @@ loginRouter.post('/', (req, res) => {
     return res.status(400).json(errors);
   }
 
-  addNewUser(req, res);
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ username }).then((user) => {
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({ usernamenotfound: 'Username not found' });
+    }
+    // Check password
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        // User matched
+        // Create JWT payload
+        const payload = {
+          id: user.id,
+          name: user.name,
+        };
+        // Sign token
+        jwt.sign(
+          payload,
+          config.secretOrKey,
+          {
+            expiresIn: 31556926,
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: `Bearer ${token}`,
+            });
+          },
+        );
+      } else {
+        return res.status(400).json({ passwordincorrect: 'Password incorrect' });
+      }
+    });
+  });
 });
 
 export default loginRouter;
