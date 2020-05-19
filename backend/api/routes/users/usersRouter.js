559621529ae5e50ef4b83549b62ba8c686c7a9f9
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-05-18 22:22:54 -0400

    new get route

--- backend/api/routes/users/usersRouter.js
+++ backend/api/routes/users/usersRouter.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import loginRouter from './loginRouter';
 import registerRouter from './registerRouter';
+import { getAllUsers } from '../../../src/controllers/UserController';
 
 const userRouter = express.Router();
+
+userRouter.get('/', getAllUsers);
 
 userRouter.use('/login', loginRouter);
 userRouter.use('/register', registerRouter);
