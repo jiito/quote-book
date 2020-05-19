cd161af53aa0763068e3156753a88bc474dffae6
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-05-18 22:22:27 -0400

    initializing passport

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ import path from 'path';
 import sassMiddleware from 'node-sass-middleware';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import passport from 'passport';
 import config from '../config';
 import apiRouter from './api';
 import serverRender from './serverRender';
+import passportConfig from './passport';
 
 const server = express();
 
@@ -31,6 +33,12 @@ mongoose.connect('mongodb://localhost/QuotesBookdb', {
 // middlewares
 server.use('/api', apiRouter);
 server.use(express.static('public'));
+
+// passport middleware
+server.use(passport.initialize());
+
+// Passport config
+passportConfig(passport);
 
 // use ejs
 server.set('view engine', 'ejs');
