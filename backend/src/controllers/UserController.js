a73a706dfca593a6deafe3b72b18eb6a08781843
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-05-18 22:23:11 -0400

    new get route

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -10,25 +10,36 @@ export const addNewUser = (req, res) => {
   User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
       return res.status(400).json({ email: 'Email already exists' });
-    } else {
-      const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-      });
-
-      // Hash password before saving in database
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser
-            .save()
-            .then((user) => res.json(user))
-            .catch((err) => console.error(err));
-        });
-      });
     }
+    console.log(req.body.password);
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    console.log(newUser);
+
+    // Hash password before saving in database
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser
+          .save()
+          .then((user) => res.json(user))
+          .catch((err) => console.error(err));
+      });
+    });
+  });
+};
+
+export const getAllUsers = (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(users);
   });
 };
 
