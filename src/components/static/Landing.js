dfe3fa0c7fe1f9e3ac7cbd0c5c8c99c06d9168a2
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-05-25 12:35:51 -0400

    first layout setup

--- src/components/static/Landing.js
+++ src/components/static/Landing.js
@@ -1,1 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Landing = () => {
+  return (
+    <div className="container align-content-center">
+      <div className="title row justify-content-center text-center">
+        <div className="col-md-10">
+          <div className="display-2">Quotd</div>
+        </div>
+      </div>
+      <div className=" row justify-content-center text-center">
+        <div className="col-sm-4">
+          <Link to="/register">
+            <button className="btn btn-primary btn-lg">Register</button>
+          </Link>
+          <Link to="/login">
+            <button className="btn btn-outline-dark btn-lg">Log In</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
