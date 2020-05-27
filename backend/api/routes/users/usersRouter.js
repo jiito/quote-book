3ca50415281c1619f86f5b4bc8d05deeb055eb73
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-05-26 22:02:33 -0400

    added delete and lookup routes

--- backend/api/routes/users/usersRouter.js
+++ backend/api/routes/users/usersRouter.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import loginRouter from './loginRouter';
 import registerRouter from './registerRouter';
-import { getAllUsers } from '../../../src/controllers/UserController';
+import { getAllUsers, deleteUserById, findUserById } from '../../../src/controllers/UserController';
 
 const userRouter = express.Router();
 
 userRouter.get('/', getAllUsers);
+userRouter.get('/:UserId', findUserById);
+userRouter.delete('/', deleteUserById);
 
 userRouter.use('/login', loginRouter);
 userRouter.use('/register', registerRouter);
