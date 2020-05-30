0f0ffd449f523470073a4e97f3856f92d34d3425
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-05-30 16:55:57 -0400

    avoid res headers error

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -11,27 +11,27 @@ export const addNewUser = (req, res) => {
     if (user) {
       return res.status(400).json({ username: 'Username already exists' });
     }
-  });
-  User.findOne({ email: req.body.email }).then((user) => {
-    if (user) {
-      return res.status(400).json({ email: 'Email already exists' });
-    }
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      username: req.body.username,
-      password: req.body.password,
-    });
+    User.findOne({ email: req.body.email }).then((user) => {
+      if (user) {
+        return res.status(400).json({ email: 'Email already exists' });
+      }
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+      });
 
-    // Hash password before saving in database
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        newUser
-          .save()
-          .then((user) => res.json(user))
-          .catch((err) => console.error(err));
+      // Hash password before saving in database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.error(err));
+        });
       });
     });
   });
