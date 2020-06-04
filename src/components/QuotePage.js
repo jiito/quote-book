27cef00568508173139ffee265ce7e66463dcaba
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-06-04 14:32:16 -0400

    initial commit

---
+++ src/components/QuotePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import QuoteList from './QuoteList';
+import NewQuoteForm from './NewQuoteForm';
+
+const QuotePage = () => (
+  <div className="container">
+    <h2 className="text-center">quotd</h2>
+    {/* <QuoteList quotes={state.quotes} onRemoveQuote={removeQuote} />
+    <NewQuoteForm onQuoteSubmit={addQuote} /> */}
+  </div>
+);
+
+export default QuotePage;
