c90ec481b0351934c5ae4d7f8b650583b9506c34
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-06-05 21:05:43 -0400

    logout

--- src/components/static/Navigation.js
+++ src/components/static/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { UserConsumer } from '../../context/UserContext';
 
 const Navigation = () => {
   return (
@@ -6,6 +7,17 @@ const Navigation = () => {
       <div className="container-fluid">
         <div className="navbar-header">
           <p className="navbar-text">The Quotebook</p>
+          <UserConsumer>
+            {({ isAuthenticated, logoutUser }) => {
+              if (isAuthenticated) {
+                return (
+                  <button className="btn btn-primary" onClick={logoutUser}>
+                    logout
+                  </button>
+                );
+              }
+            }}
+          </UserConsumer>
         </div>
       </div>
     </nav>
